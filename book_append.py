575731f6f1fe0f80e57e2e94a179e6a7c8a71e75
RyuJunho <hoo9902@naver.com>
2022-04-17 15:10:54 +0900

    main화면에서 book_append, book_search 호출 하기

--- book_append.py
+++ book_append.py
@@ -3,124 +3,129 @@ from tkinter.ttk import *
 from tkinter import scrolledtext
 from tkinter import messagebox
 
-# 추가 버튼을 클릭했을 때 호출되는 이벤트 핸들러
-def append():
-    print('추가 버튼 클릭')
-    #빈칸이 있으면
-        #메시지박스 출력 후 등록화면창으로 돌아감
-    if len(B_nameEntry.get())==0 or len(B_PubEntry.get())==0 or len(B_ISBNEntry.get())==0 or len(B_LinkEntry.get())==0 or len(B_PriEntry.get())==0 or len(B_WirEntry.get())==0 or len(B_Intrscr.get("1.0", "end-1c")) == 0:
-        AppErBox = messagebox.showerror("미입력", "빈칸이 존재합니다.\n빈칸을 입력하세요.")
-    
-    #ISBN 중복 되면 (아직 구현 x)
-        #메시지박스 출력 후 등록화면창으로 돌아감
 
-    #등록을 묻는 메시지박스 출력 (예, 아니오)
-    else:
-        AppCheckBox = messagebox.askokcancel("도서등록", "도서를 등록하시겠습니까?")
-    #'예'를 누를경우
-        #등록되었다는 메시지박스 출력 (확인)
-        #엔트리와 텍스트의 내용을 비움
-        if AppCheckBox == 1:
-            AppYesBox = messagebox.showinfo("등록완료", "등록되었습니다.")
-            B_nameEntry.delete(0,"end")
-            B_PubEntry.delete(0,"end")
-            B_ISBNEntry.delete(0,"end")
-            B_LinkEntry.delete(0,"end")
-            B_WirEntry.delete(0,"end")
-            B_PriEntry.delete(0,"end")
-            B_Intrscr.delete("1.0", "end")
-        #'아니오'를 누를경우
-            #등록화면창으로 돌아감 (내용을 비우지 않음)
+
+def book_append(main) :    #도서등록(매개변수 = 초기화면)  #나중에 다른파일과 함수로 연결할거임
+
+    # 추가 버튼을 클릭했을 때 호출되는 이벤트 핸들러
+    def append():
+        print('추가 버튼 클릭')
+        # 빈칸이 있으면
+        # 메시지박스 출력 후 등록화면창으로 돌아감
+        if len(B_nameEntry.get()) == 0 or len(B_PubEntry.get()) == 0 or len(B_ISBNEntry.get()) == 0 or len(
+                B_LinkEntry.get()) == 0 or len(B_PriEntry.get()) == 0 or len(B_WirEntry.get()) == 0 or len(
+                B_Intrscr.get("1.0", "end-1c")) == 0:
+            AppErBox = messagebox.showerror("미입력", "빈칸이 존재합니다.\n빈칸을 입력하세요.")
+
+        # ISBN 중복 되면 (아직 구현 x)
+        # 메시지박스 출력 후 등록화면창으로 돌아감
+
+        # 등록을 묻는 메시지박스 출력 (예, 아니오)
         else:
-            AppNoBox = messagebox.showinfo("등록취소", "등록이 취소 되었습니다.")
+            AppCheckBox = messagebox.askokcancel("도서등록", "도서를 등록하시겠습니까?")
+            # '예'를 누를경우
+            # 등록되었다는 메시지박스 출력 (확인)
+            # 엔트리와 텍스트의 내용을 비움
+            if AppCheckBox == 1:
+                AppYesBox = messagebox.showinfo("등록완료", "등록되었습니다.")
+                B_nameEntry.delete(0, "end")
+                B_PubEntry.delete(0, "end")
+                B_ISBNEntry.delete(0, "end")
+                B_LinkEntry.delete(0, "end")
+                B_WirEntry.delete(0, "end")
+                B_PriEntry.delete(0, "end")
+                B_Intrscr.delete("1.0", "end")
+            # '아니오'를 누를경우
+            # 등록화면창으로 돌아감 (내용을 비우지 않음)
+            else:
+                AppNoBox = messagebox.showinfo("등록취소", "등록이 취소 되었습니다.")
 
+    frame = Frame(main)
 
-# 임시화면
-main = Tk()
-main.geometry("800x500")
-# def book_append(main) :    #도서등록(매개변수 = 초기화면)  #나중에 다른파일과 함수로 연결할거임
+    # '도서등록' 레이블 생성 글자크기 설정
+    # '도서등록' 레이블을 main에 부착
+    B_ApeLabel = Label(frame, text ="도서등록", font=(None,12))
+    B_ApeLabel.grid(row=1,column=1,pady=10)
 
-# '도서등록' 레이블 생성 글자크기 설정
-# '도서등록' 레이블을 main에 부착
-B_ApeLabel = Label(main, text ="도서등록", font=(None,12))
-B_ApeLabel.pack(side=TOP, ipadx=320, pady=20)
+    B_frame_1 = Frame(frame)
+    B_frame_1.grid(row=2,column=1)
 
-# '도서명' 레이블 생성
-# '도서명' 레이블을 main에 부착
-B_nameLabel = Label(main, text="도서명")
-B_nameLabel.place(x = 180,y = 60)
+    # '도서명' 레이블 생성
+    # '도서명' 레이블을 main에 부착
+    B_nameLabel = Label(B_frame_1, text="도서명")
+    B_nameLabel.grid(row=1,column=1,padx=10 ,pady=10)
 
-# '도서명' 엔트리(텍스트박스) 생성
-# '도서명' 엔트리를 main에 부착
-B_nameEntry = Entry(main, width=50)
-B_nameEntry.place(x = 240,y = 60)
+    # '도서명' 엔트리(텍스트박스) 생성
+    # '도서명' 엔트리를 main에 부착
+    B_nameEntry = Entry(B_frame_1, width=50)
+    B_nameEntry.grid(row=1,column=2)
 
-# '출판사' 레이블 생성
-# '출판사' 레이블을 main에 부착
-B_PubLabel = Label(main, text ="출판사")
-B_PubLabel.place(x = 180,y = 100)
+    # '출판사' 레이블 생성
+    # '출판사' 레이블을 main에 부착
+    B_PubLabel = Label(B_frame_1, text ="출판사")
+    B_PubLabel.grid(row=2,column=1,pady=10)
 
-# '출판사' 엔트리(텍스트박스) 생성
-# '출판사' 엔트리를 main에 부착
-B_PubEntry = Entry(main, width=50)
-B_PubEntry.place(x = 240,y = 100)
+    # '출판사' 엔트리(텍스트박스) 생성
+    # '출판사' 엔트리를 main에 부착
+    B_PubEntry = Entry(B_frame_1, width=50)
+    B_PubEntry.grid(row=2,column=2)
 
-# 'ISBN' 레이블 생성
-# 'ISBN' 레이블을 main에 부착
-B_ISBNLabel = Label(main, text ="ISBN")
-B_ISBNLabel.place(x = 180,y = 140)
+    # 'ISBN' 레이블 생성
+    # 'ISBN' 레이블을 main에 부착
+    B_ISBNLabel = Label(B_frame_1, text ="ISBN")
+    B_ISBNLabel.grid(row=3,column=1,pady=10)
 
-# 'ISBN' 엔트리(텍스트박스) 생성
-# 'ISBN' 엔트리를 main에 부착
-B_ISBNEntry = Entry(main, width=50)
-B_ISBNEntry.place(x = 240,y = 140)
+    # 'ISBN' 엔트리(텍스트박스) 생성
+    # 'ISBN' 엔트리를 main에 부착
+    B_ISBNEntry = Entry(B_frame_1, width=50)
+    B_ISBNEntry.grid(row=3,column=2)
 
-# '관련링크' 이블 생성
-# '관련링크' 레이블을 main에 부착
-B_LinkLabel = Label(main, text ="관련링크")
-B_LinkLabel.place(x = 180,y = 180)
+    # '관련링크' 이블 생성
+    # '관련링크' 레이블을 main에 부착
+    B_LinkLabel = Label(B_frame_1, text ="관련링크")
+    B_LinkLabel.grid(row=4,column=1,pady=10)
 
-# '관련링크' 엔트리(텍스트박스) 생성
-# '관련링크' 엔트리를 main에 부착
-B_LinkEntry = Entry(main, width=50)
-B_LinkEntry.place(x = 240,y = 180)
+    # '관련링크' 엔트리(텍스트박스) 생성
+    # '관련링크' 엔트리를 main에 부착
+    B_LinkEntry = Entry(B_frame_1, width=50)
+    B_LinkEntry.grid(row=4,column=2)
 
-# '저자' 레이블 생성
-# '저자' 레이블을 main에 부착
-B_WirLabel = Label(main, text ="저자")
-B_WirLabel.place(x = 180,y = 220)
+    # '저자' 레이블 생성
+    # '저자' 레이블을 main에 부착
+    B_WirLabel = Label(B_frame_1, text ="저자")
+    B_WirLabel.grid(row=5,column=1,pady=10)
 
-# '저자' 엔트리(텍스트박스) 생성
-# '저자' 엔트리를 main에 부착
-B_WirEntry = Entry(main, width=20)
-B_WirEntry.place(x = 240,y = 220)
+    B_frame_2 = Frame(B_frame_1)
+    B_frame_2.grid(row=5,column=2)
 
-# '가격' 레이블 생성
-# '가격' 레이블을 main에 부착
-B_PriLabel = Label(main, text ="가격")
-B_PriLabel.place(x = 420,y = 220)
+    # '저자' 엔트리(텍스트박스) 생성
+    # '저자' 엔트리를 main에 부착
+    B_WirEntry = Entry(B_frame_2, width=23)
+    B_WirEntry.grid(row=1,column=1)
 
-# '가격' 엔트리(텍스트박스) 생성
-# '가격' 엔트리를 main에 부착
-B_PriEntry = Entry(main, width=16)
-B_PriEntry.place(x = 478,y = 220)
+    # '가격' 레이블 생성
+    # '가격' 레이블을 main에 부착
+    B_PriLabel = Label(B_frame_2, text ="가격")
+    B_PriLabel.grid(row=1,column=2,padx=10)
 
-# '도서소개' 레이블 생성
-# '도서소개' 레이블을 main에 부착
-B_IntrLabel = Label(main, text ="도서소개")
-B_IntrLabel.pack(side=LEFT, padx=40, pady=20)
+    # '가격' 엔트리(텍스트박스) 생성
+    # '가격' 엔트리를 main에 부착
+    B_PriEntry = Entry(B_frame_2, width=20)
+    B_PriEntry.grid(row=1,column=3)
 
-# '도서소개' 스크롤텍스트(ScrolledText) 생성
-# '도서소개' 스크롤텍스트(ScrolledText) main에 부착
-B_Intrscr = scrolledtext.ScrolledText(main, width=100, height=10, wrap=WORD)
-B_Intrscr.place(x = 40,y = 300)
+    # '도서소개' 레이블 생성
+    # '도서소개' 레이블을 main에 부착
+    B_IntrLabel = Label(frame, text ="도서소개")
+    B_IntrLabel.grid(row=3,column=1,pady=10)
 
-# '추가'버튼 생성 (command = append)
-# 버튼을 main 부착
-B_AppButton = Button(main,text="추가", width=8, command=append)
-B_AppButton.place(x = 320,y = 450)
+    # '도서소개' 스크롤텍스트(ScrolledText) 생성
+    # '도서소개' 스크롤텍스트(ScrolledText) main에 부착
+    B_Intrscr = scrolledtext.ScrolledText(frame, width=100, height=10, wrap=WORD)
+    B_Intrscr.grid(row=4,column=1)
 
-B_CancButton = Button(main,text="취소", width=8)
-B_CancButton.place(x = 420,y = 450)
+    # '추가'버튼 생성 (command = append)
+    # 버튼을 main 부착
+    B_AppButton = Button(frame,text="추가", width=8, command=append)
+    B_AppButton.grid(row=5,column=1,pady=30)
 
-main.mainloop()
+    return frame
--- main.py
+++ main.py
@@ -1,40 +1,54 @@
 from tkinter import *
+from book_append import *
+from book_search import *
+
 
 #1주차 회의록을 참조하여 설계 할 것
 #GUI 메인화면을 이 파일에서 설계
 #이벤트 처리기 기능은 구현은 하지말고 주석으로 설명 달아놓을 것
 
 # 이벤트 핸들러 ##################################################################################
-def book_search():  #도서검색 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
-    print('도서검색 메뉴')
 
-def book_signup():  #도서등록 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
-    print('도서등록 메뉴')
+def main_book_search(main):  #도서검색 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+    global frame
+    frame.destroy()                 #기존화면 제거
+    new_frame = book_search(main)   #도서검색 화면 가져오기
+    frame = new_frame               #도서검색 화면으로 교체
+    frame.pack()                    #도서검색 화면 띄우기
+    #print('도서검색 메뉴')
 
-def book_correct():  #도서수정 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+def main_book_append(main):  #도서등록 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+    global frame
+    frame.destroy()                 #기존화면 제거
+    new_frame = book_append(main)   #도서등록 화면 가져오기
+    frame = new_frame               #도서등록 화면으로 교체
+    frame.pack()                    #도서등록 화면 띄우기
+    #print('도등록 메뉴')
+
+def main_book_modify():  #도서수정 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
     print('도서수정 메뉴')
 
-def book_drop():  #도서삭제 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+def main_book_delete():  #도서삭제 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
     print('도서삭제 메뉴')
 
 
-def user_search():  #회원검색 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+def main_user_search():  #회원검색 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
     print('회원검색 메뉴')
 
-def user_signup():  #회원등록 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+def main_user_append():  #회원등록 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
     print('회원등록 메뉴')
 
-def user_correct():  #회원수정 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+def main_user_modify():  #회원수정 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
     print('회원수정 메뉴')
 
-def user_drop():  #회원탈퇴 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+def main_user_delete():  #회원탈퇴 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
     print('회원탈퇴 메뉴')
 
 
-def checkout_():  #대출하기 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+def main_checkout_():  #대출하기 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
     print('대출하기 메뉴')
 
-def return_():  #반납하기 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
+def main_return_():  #반납하기 메뉴를 클릭하였을 때 호출될 이벤트 핸들러
     print('반납하기 메뉴')
 
 # 이벤트 핸들러 -끝- ##############################################################################
@@ -49,34 +63,37 @@ x = (sw/2) - (width/2)
 y = (sh/2) - (height/2)
 main.geometry('%dx%d+%d+%d' % (width, height, x, y)) #메인창 크기 고정, 중앙에 출력되도록 설정
 
+global frame
+frame = Frame(main)
 
 # 메뉴 ############################################################################################
 menubar = Menu(main) #메뉴바 생성
 
 book_menu = Menu(menubar, tearoff=0) #도서관리(상위메뉴) 생성
 menubar.add_cascade(label='도서관리', menu=book_menu)
-book_menu.add_command(label='도서검색', command=book_search()) #도서검색(하위메뉴) 생성  (각 하위메뉴 사이에는 구분선을 추가 할 것)
-book_menu.add_command(label='도서등록', command=book_signup()) #도서등록(하위메뉴) 생성
+book_menu.add_command(label='도서검색', command= lambda : main_book_search(main)) #도서검색(하위메뉴) 생성  (각 하위메뉴 사이에는 구분선을 추가 할 것)
+book_menu.add_command(label='도서등록', command = lambda : main_book_append(main)) #도서등록(하위메뉴) 생성
 book_menu.add_separator()
-book_menu.add_command(label='도서수정', command=book_correct()) #도서수정(하위메뉴) 생성
-book_menu.add_command(label='도서삭제', command=book_drop()) #도서삭제(하위메뉴) 생성
+book_menu.add_command(label='도서수정', command= lambda : main_book_modify) #도서수정(하위메뉴) 생성
+book_menu.add_command(label='도서삭제', command= lambda : main_book_delete) #도서삭제(하위메뉴) 생성
 
 user_menu = Menu(menubar, tearoff=0) #회원관리(상위메뉴) 생성
 menubar.add_cascade(label='회원관리', menu=user_menu)
-user_menu.add_command(label='회원검색', command=user_search()) #회원검색(하위메뉴) 생성
-user_menu.add_command(label='회원등록', command=user_signup()) #회원등록(하위메뉴) 생성
+user_menu.add_command(label='회원검색', command= lambda : main_user_search) #회원검색(하위메뉴) 생성
+user_menu.add_command(label='회원등록', command= lambda : main_user_append) #회원등록(하위메뉴) 생성
 user_menu.add_separator()
-user_menu.add_command(label='회원수정', command=user_correct()) #회원수정(하위메뉴) 생성
-user_menu.add_command(label='회원탈퇴', command=user_drop()) #회원탈퇴(하위메뉴) 생성
+user_menu.add_command(label='회원수정', command= lambda : main_user_modify) #회원수정(하위메뉴) 생성
+user_menu.add_command(label='회원탈퇴', command= lambda : main_user_delete) #회원탈퇴(하위메뉴) 생성
 
 checkout_menu = Menu(menubar, tearoff=0) #대출/반납(상위메뉴) 생성
 menubar.add_cascade(label='대출/반납', menu=checkout_menu)
-checkout_menu.add_command(label='대출하기', command=(checkout_())) #대출하기(하위메뉴) 생성
-checkout_menu.add_command(label='반납하기', command=(return_()))#반납하기(하위메뉴) 생성
+checkout_menu.add_command(label='대출하기', command= lambda : main_checkout_) #대출하기(하위메뉴) 생성
+checkout_menu.add_command(label='반납하기', command= lambda : main_return_)#반납하기(하위메뉴) 생성
 
 main.config(menu=menubar)   #메뉴바 부착
 
 # 메뉴 -끝- #########################################################################################
 
 main.mainloop() #실행
+
 
