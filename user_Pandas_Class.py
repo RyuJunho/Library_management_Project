0296a21ac7343cc51239525831a2e232250edbe4
has8781 <has8781@gmail.com>
2022-04-24 05:46:06 +0900

    회원 검색, 등록 이벤트 처리

--- user_Pandas_Class.py
+++ user_Pandas_Class.py
@@ -1,50 +1,44 @@
 import numpy as np
 import pandas as pd
-
+from tkinter import messagebox
 
 class Main:
     def __init__(self, user): # self에 인스턴스 전달되어 있음. . 그래서 변수작성이나 참고 가능하고. . 자동 호출
         self.USER = pd.read_csv(user).sort_values(by=['이름'], axis=0) # 데이터 프레임
 
-
-    def user_search(self, cho): # 회원 검색
-        if cho == '이름':
-            ins = input('이름을 입력하세요. : ')
-            if (self.USER['이름'] == ins).any():
-                search = np.array(self.USER.loc[self.USER['이름'].str.contains(ins), ['전화번호', '이름', '생년월일', '성별', '이메일', '탈퇴여부']]) # contains 특정단어검색 loc 행단위 출력
-                return search
-            else:
-                print('등록되어 있지 않은 회원입니다.\n')
-        elif cho == '전화번호':
-            ins = input('전화번호를 입력하세요. : ')
-            if (self.USER['전화번호'] == ins).any():
-                search = np.array(self.USER.loc[self.USER['전화번호'].str.contains(ins), ['전화번호', '이름', '생년월일', '성별', '이메일', '탈퇴여부']])
-                return search
-            else:
-                print('등록되어 있지 않은 회원입니다.\n')
-
-
-    def user_append(self, phone, name, birth, sex, email): # 회원 등록
-        ins = input('전화번호를 입력하세요. : ')
-        if (self.USER['전화번호'] == ins).any(): # 전화번호 중복 검색
-            print('중복된 전화번호입니다.\n')
+    def user_search(self, combox, in_nanum): # 회원 검색
+        if combox == '이름':
+            search = np.array(self.USER.loc[self.USER['이름'].str.contains(in_nanum), ['이름', '전화번호', '성별', '탈퇴여부']]) # contains 특정단어검색 loc 행단위 출력
+            print('이름')
+            return search
+        elif combox == '전화번호':
+            search = np.array(self.USER.loc[self.USER['전화번호'].str.contains(in_nanum), ['이름', '전화번호', '성별', '탈퇴여부']])
+            return search
         else:
-            append = pd.DataFrame({'전화번호': phone, '이름': name, '생년월일': birth, '성별': sex, '이메일': email, '탈퇴여부':True})
+            messagebox.showerror('알림', '잘못 입력하셨습니다.')
+
+    def user_append(self, name, birth, sex, phone, email): # 회원 등록
+        if (self.USER['전화번호'] == phone).any(): # 전화번호 중복 검색
+            return True
+        else:
+            if sex == 1: # 성별
+                sex = False
+                return sex
+            elif sex == 2:
+                sex = True
+                return sex
+            append = pd.DataFrame({'이름': name, '생년월일': birth, '성별': sex, '전화번호': phone, '이메일': email, '탈퇴여부':True})
             self.USER = pd.concat([self.USER, append]).sort_values(by='이름', axis=0)
             self.USER.to_csv('USER_list.csv', index=None)
 
-
-    def user_modify(self, phone, name, birth, sex, email): # 회원 수정
-        ins = input('전화번호를 입력하세요. : ')
+    def user_modify(self, ins, phone, name, birth, sex, email): # 회원 수정
         if (self.USER['전화번호'] == ins).any():  # 전화번호 검색
             self.USER.loc[self.USER['전화번호'].str.contains(ins), ('전화번호', '이름', '생년월일', '성별', '이메일')] = (phone, name, birth, sex, email)
             self.USER.to_csv('USER_list.csv', index=None)
         else:
             print('등록되지 않은 회원입니다.\n')
 
-
-    def user_delete(self): # 회원 탈퇴
-        ins = input('전화번호를 입력하세요. : ')
+    def user_delete(self, ins): # 회원 탈퇴
         if (self.USER['전화번호'] == ins).any():
             if (self.USER['대여여부'] == True).any(): # 대여여부 관련 물어보고 수정
                 print('책을 대여 중인 회원입니다.\n')
--- user_append.py
+++ user_append.py
@@ -1,40 +1,39 @@
 from tkinter import *
 from tkinter.ttk import *
+from user_Pandas_Class import*
 from tkinter import messagebox
 
 
+def user_append(main): #회원등록(매개변수 = 초기화면)
 
-def user_append(main):    #회원등록(매개변수 = 초기화면)  #나중에 다른파일과 함수로 연결할거임
+    def append(): # 추가 버튼을 클릭했을 때 호출되는 이벤트 핸들러
 
-    # 추가 버튼을 클릭했을 때 호출되는 이벤트 핸들러
-    def append():
-        print('추가 버튼 클릭')
-        # 빈칸이 있으면
+
         if len(entry_name.get()) == 0 or len(entry_birth.get()) == 0 or len(entry_num1.get()) == 0 or len(
-                entry_num2.get()) == 0 or len(entry_num3.get()) == 0 or len(entry_mail.get()) == 0:
+                entry_num2.get()) == 0 or len(entry_num3.get()) == 0 or len(entry_mail.get()) == 0: # 빈칸이 있으면
             messagebox.showinfo('도서 관리 프로그램 메시지', '빈칸이 존재합니다.')  # 메시지박스 출력 후 등록화면창으로 돌아감
-
-        # elif (): # 전화번호가 중복 되면 (아직 구현 x)
-        # messagebox.showinfo("도서 관리 프로그램 메시지", "중복된 전화번호입니다.") # 메시지박스 출력 후 등록화면창으로 돌아감
 
         else:
             check_yn = messagebox.askokcancel('도서 관리 프로그램 메시지', '회원을 등록하시겠습니까?')  # 등록을 묻는 메시지박스 출력 (예, 아니오)
             if check_yn == 1:  # '예'를 누를경우
-                messagebox.showinfo('알림', '등록되었습니다.')  # 등록되었다는 메시지박스 출력 (확인)
-                # 엔트리와 텍스트의 내용을 비움
-                entry_name.delete(0, 'end')
-                entry_birth.delete(0, 'end')
-                entry_num1.delete(0, 'end')
-                entry_num2.delete(0, 'end')
-                entry_num3.delete(0, 'end')
-                entry_mail.delete(0, 'end')
-
+                user_pi = Main('USER_list.csv')
+                ph_num = str(entry_num1.get()) + '-' + str(entry_num2.get()) + '-' + str(entry_num3.get()) # str로 전화번호 저장
+                us_list = user_pi.user_append(entry_name.get(), entry_birth.get(), radio_sex.get(), ph_num, entry_mail.get())
+                if us_list:
+                    messagebox.showinfo("도서 관리 프로그램 메시지", "이미 등록된 전화번호입니다.")  # 메시지박스 출력 후 등록화면창으로 돌아감
+                else:
+                    messagebox.showinfo('알림', '등록되었습니다.')  # 등록되었다는 메시지박스 출력 (확인)
+                    entry_name.delete(0, 'end')  # 엔트리와 텍스트의 내용을 비움
+                    entry_birth.delete(0, 'end')
+                    entry_num1.delete(0, 'end')
+                    entry_num2.delete(0, 'end')
+                    entry_num3.delete(0, 'end')
+                    entry_mail.delete(0, 'end')
             else:  # '아니오'를 누를경우
                 messagebox.showinfo('알림', '등록이 취소되었습니다.')  # 메시지 박스를 뜨운 후 등록화면창으로 돌아감 (내용을 비우지 않음)
 
 
     frame = Frame(main)
-
 
     label_append = Label(frame, text='회원등록') # '회원등록' 레이블 생성 글자크기 설정
     label_append.grid(row=1, column=1, pady=50)
@@ -48,13 +47,11 @@ def user_append(main):    #회원등록(매개변수 = 초기화면)  #나중에
     entry_name = Entry(text_frame, width=30) # '이름' 엔트리(텍스트박스) 생성
     entry_name.grid(row=2, column=2)
 
-
     label_birth = Label(text_frame, text='생년월일', width=10) # '생년월일' 레이블 생성
     label_birth.grid(row=3, column=1, pady=10)
 
     entry_birth = Entry(text_frame, width=30) # '생년월일' 엔트리(텍스트박스) 생성
     entry_birth.grid(row=3, column=2)
-
 
     sex_frame = Frame(text_frame)
     sex_frame.grid(row=4, column=2)
@@ -65,7 +62,6 @@ def user_append(main):    #회원등록(매개변수 = 초기화면)  #나중에
     radio_man = Radiobutton(sex_frame, text='남자', value=2, variable=radio_sex)
     radio_wo.grid(row=1, column=1)
     radio_man.grid(row=1, column=2, padx=60)
-
 
     num_frame = Frame(text_frame)
     num_frame.grid(row=5, column=2)
@@ -88,16 +84,13 @@ def user_append(main):    #회원등록(매개변수 = 초기화면)  #나중에
     entry_num3 = Entry(num_frame, width=9) # '전화번호' 세번째 엔트리(텍스트박스) 생성
     entry_num3.grid(row=1, column=5) # '전화번호' 세번째 엔트리를 main에 부착
 
-
     label_mail = Label(text_frame, text='이메일', width=10)# '이메일' 레이블 생성
     label_mail.grid(row=6, column=1, pady=10) # '이메일' 레이블을 main에 부착
 
     entry_mail = Entry(text_frame, width=30) # '이메일' 엔트리(텍스트박스) 생성
     entry_mail.grid(row=6, column=2) # '이메일' 엔트리를 main에 부착
 
-
     us_plus = Button(frame, text='추가', width=5, command=append) # '추가'버튼 생성 (command = append)
     us_plus.grid(row=3, column=1, pady=80)  # 버튼을 main 부착
-
 
     return frame
