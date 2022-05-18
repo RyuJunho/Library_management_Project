c1d73e9250be70f9f06a9eb793a4915db40531f2
JUNG\alswj <20212856@office.deu.ac.kr>
2022-05-18 20:59:58 +0900

    대여 중인 회원 트리뷰 나오게 수정

--- user_Pandas_Class.py
+++ user_Pandas_Class.py
@@ -97,7 +97,7 @@ class Main:
     def user_rent(self, phone):  # 회원 탈퇴 - 도서 트리뷰 추가
         del_tree = self.RENT[(self.RENT['전화번호'] == phone)]
         #if (del_tree['전화번호'] == phone).any():
-        search = np.array(self.RENT.loc[self.RENT['전화번호'].str.contains(phone), ['제목', '반납예정일']])
+        search = np.array(del_tree.loc[del_tree['전화번호'].str.contains(phone), ['제목', '반납예정일']])
         return search
         #else:
         #    print('대여중인 도서가 없습니다.')
